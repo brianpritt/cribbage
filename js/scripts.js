f2eccf9c35fb8461a10946d5942f09f3009edccb
Brian Pritt and daniel munger <daniel@>
2016-11-07 11:08:16 -0800

    add Deck Prototype for shuffle().

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,19 @@ function Card (suit, rank, cardValue) {
   this.played = false;
 
 }
+
+Deck.prototype.shuffleDeck(){
+  //This is the Fisher-Yates shuffle
+  var m = this.cards.length, t, i;
+  //while there are cards still to be shuffled
+  while(m) {
+    //takes a random element
+    i = Math.floor(Math.random() * m--);
+    //swap it with the current element
+    t = this.cards[m];
+    this.cards[m] = this.cards[i];
+    this.cards[i] = t;
+  }
+  //return the shuffled deck.
+  return this.cards
+};
