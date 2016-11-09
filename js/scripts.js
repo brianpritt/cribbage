e84f05aebcd23fd268530fa99bc34eb37d5ce52a
Brian Pritt and Daniel Munger <daniel@>
2016-11-09 14:29:12 -0800

    get new round cards clickable

--- js/scripts.js
+++ js/scripts.js
@@ -235,13 +235,12 @@ Game.prototype.roundOver = function(){
     this.clearTable();
     $("#player1, #player2").show();
     $("#player1Highlight, #player2Highlight").removeClass("highlightCurrent");
-    $("div.cards").off("click");
+    $("#player1,#player2 ").children().removeClass("cards");
+
     $("#newGame").show();
 
+  };
 };
-};
-
-Game.prototype.addClickability = function(){}
 
 Game.prototype.addToTableDisplay = function() {
   var target = this;
@@ -336,7 +335,7 @@ $(document).ready(function(){
       $("#turnt").html("<img src="+turntCard.cardImage+">");
     }
   });
-
+  var crick;
   $("#startGame").click(function(){
     $("#gameStart").hide();
     $("#gameField").show();
@@ -346,24 +345,24 @@ $(document).ready(function(){
     game.players[0].cribCheckbox();
     game.players[1].displayHand();
 
-      $(".cards").click(function(){
-        if(game.crib.length === 4){
-          var selectedCard = parseInt($(this).attr("value"));
-          var clickedCard = game.currentPlayer.hand[selectedCard];
-          game.toTable(clickedCard);
-          $("#tableScore").text("Current Table  Score: " + game.tableScore);
-          game.roundOver();
-        };
-      });
-      $(".goBtn").click(function(){
-        game.goPlayer();
-        $("#tableScore").text("Current Table Score: " + game.tableScore);
-      });
+    $(".cards").click(function(){
+      if(game.crib.length === 4){
+        var selectedCard = parseInt($(this).attr("value"));
+        var clickedCard = game.currentPlayer.hand[selectedCard];
+        game.toTable(clickedCard);
+        $("#tableScore").text("Current Table  Score: " + game.tableScore);
+        game.roundOver();
+      };
+    });
+    $(".goBtn").click(function(){
+      game.goPlayer();
+      $("#tableScore").text("Current Table Score: " + game.tableScore);
+    });
   });
   $("#newGame").click(function(){
     game.clearTable();
     game.newRound();
-    $("div.cards").on("click");
-
-  });
+    $("#player1, #player2").children().addClass("cards");
+    $("#cribShow").hide();
+    });
 });
